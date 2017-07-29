d89008876f8775551df7682dd970cdc729c38389
AIZAWA Hina <hina@bouhime.com>
2017-07-29 15:52:05 +0900

    db
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- config/db.php
+++ config/db.php
@@ -1,9 +1,5 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'sqlite:' . dirname(__DIR__) . '/database/db.sqlite',
 ];
---
+++ models/Language.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "language".
+ *
+ * @property integer $id
+ * @property string $code
+ * @property string $name
+ */
+class Language extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'language';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['code'], 'string', 'max' => 8],
+            [['name'], 'string', 'max' => 32],
+            [['name'], 'unique'],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+        ];
+    }
+}
