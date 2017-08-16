dd435ee8ccc8ca2a57caa25c96d086a7809e8379
AIZAWA Hina <hina@bouhime.com>
2017-08-16 18:34:43 +0900

    update API doc
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- views/site/index.php
+++ views/site/index.php
@@ -14,4 +14,61 @@ $this->title = 'SPLAPI2';
   <p>
     現在から未来にかけてのスケジュール情報を一括で返します。(application/json)
   </p>
+  <pre>
+{
+  "regular": [ // レギュラーマッチ
+    {
+      "mode": {  
+        "key": "nawabari", // stat.ink のキーと共通
+        "name": {  
+          "en-GB": "Turf War",
+          "en-US": "Turf War",
+          "ja-JP": "ナワバリバトル"
+        }
+      },
+      "start": { // 開始日時
+        "unixtime": 1502870400, // 秒精度
+        "iso8601": "2017-08-16T08:00:00+00:00" // ISO-8601拡張形式準拠。タイムゾーン不定。
+      },
+      "end": { // 終了日時
+        "unixtime":1502877600,
+        "iso8601":"2017-08-16T10:00:00+00:00"
+      },
+      "stages": [ 
+        {  
+          "splatnet": 1, // イカリング2のIDと共通
+          "key": "fujitsubo", // stat.inkのキーと共通(may null)
+          "name": {
+            "en-GB":"Musselforge Fitness",
+            "en-US":"Musselforge Fitness",
+            "ja-JP":"フジツボスポーツクラブ"
+          },
+          "image": "https://splapi2.stat.ink/images/stage/211ae5e0-c6bc-5a5c-be9e-75e4f2a07dee.png"
+        },
+        {  
+          "splatnet": 7,
+          "key": "hokke",
+          "name":{  
+            "en-GB": "Port Mackerel",
+            "en-US": "Port Mackerel",
+            "ja-JP": "ホッケふ頭"
+          },
+          "image":"https://splapi2.stat.ink/images/stage/b57039a1-f9c5-5a99-93ad-db46d168926c.png"
+        }
+      ]
+    },
+    // ... repeat ...
+  ],
+  "gachi": [ // ガチマッチ
+    // see "regular"
+  ],
+  "league": [ // リーグマッチ
+    // see "regular"
+  ],
+  "fest": [] // フェスマッチ用データは現在未サポートなので必ず何も返らない
+}
+  </pre>
+  <p>
+    <code>mode.key</code>は次のいずれか: <code>nawabari</code>, <code>area</code>, <code>yagura</code>, <code>hoko</code>
+  </p>
 </div>
